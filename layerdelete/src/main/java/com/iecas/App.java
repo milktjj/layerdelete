462931ad5fabe079bb292688f893ea96817f26a4
MILK <milktjj@gmail.com>
2019-06-04 17:08:53 +0800

    no message

--- layerdelete/src/main/java/com/iecas/App.java
+++ layerdelete/src/main/java/com/iecas/App.java
@@ -1,13 +1,26 @@
 package com.iecas;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.iecas.utils.QuadTreeUtil;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import com.iecas.utils.XMLP;
+
+import javax.xml.parsers.DocumentBuilder;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+    public static void main(String[] args) throws MalformedURLException, DocumentException {
+        long[] xyz = new long[3];
+        xyz[0] = 2L;
+        xyz[1] = 23L;
+        xyz[2] = 10L;
+        System.out.println(QuadTreeUtil.xyz2QuadTreeCode(xyz));
+        Document document = XMLP.parse(new URL("file:///C:\\Users\\MILK\\Desktop\\wmts.xml"));
+        //XMLP.getLayerNames(document);
+        System.out.println(XMLP.getLayerInfo(document, "EPSG:4326", "DESP:全国SRTM拼接", 13)[1]);
     }
 }
---
+++ layerdelete/src/main/java/com/iecas/utils/XMLP.java
@@ -0,0 +1,66 @@
+package com.iecas.utils;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.net.URL;
+import java.util.Iterator;
+
+public class XMLP {
+    public static Document parse(URL url) throws DocumentException {
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(url);
+        return document;
+    }
+
+    public static int[] getLayerInfo(Document document, String matrix,String layerName,int z) throws DocumentException {
+
+        Element root = document.getRootElement();
+        int[] limit = new int[4];
+        for (Iterator<Element> it = root.element("Contents").elementIterator(); it.hasNext(); ) {
+            Element foo = it.next();
+            if (foo.getName().equals("Layer")) {
+                // iterate through attributes of root
+                if (foo.element("Identifier").getStringValue().equals(layerName)) {
+                    for (Iterator<Element> matruxSet_it = foo.elementIterator("TileMatrixSetLink"); matruxSet_it.hasNext(); ) {
+                        Element EPSG = matruxSet_it.next();
+                        if (EPSG.element("TileMatrixSet").getStringValue().equals(matrix)) {
+                            for (Iterator<Element> EPSG_4326_it = EPSG.element("TileMatrixSetLimits").elementIterator(); EPSG_4326_it.hasNext(); ) {
+                                Element element = EPSG_4326_it.next();
+                                // do something
+                                if (element.getName().equals("TileMatrixLimits")) {
+                                    if (element.element("TileMatrix").getStringValue().equals(matrix + ":" + z)) {
+                                        limit[0] = Integer.parseInt(element.element("MinTileRow").getStringValue());
+                                        limit[1] = Integer.parseInt(element.element("MaxTileRow").getStringValue());
+                                        limit[2] = Integer.parseInt(element.element("MinTileCol").getStringValue());
+                                        limit[3] = Integer.parseInt(element.element("MaxTileCol").getStringValue());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return limit;
+
+    }
+
+    public static void getLayerNames(Document document) throws DocumentException {
+
+        Element root = document.getRootElement();
+
+        for (Iterator<Element> it = root.element("Contents").elementIterator(); it.hasNext(); ) {
+            Element foo = it.next();
+            if (foo.getName().equals("Layer")) {
+                // iterate through attributes of root
+                System.out.println(foo.element("Identifier").getStringValue());
+            }
+        }
+
+
+    }
+}
