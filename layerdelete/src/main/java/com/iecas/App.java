6745980f1290ed14fa32afb70f0582ae0f8a9eb7
MILK <milktjj@gmail.com>
2019-06-12 09:23:02 +0800

    612-1

--- layerdelete/src/main/java/com/iecas/App.java
+++ layerdelete/src/main/java/com/iecas/App.java
@@ -15,37 +15,42 @@ import javax.xml.parsers.DocumentBuilder;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String url = "file:///C:\\Users\\MILK\\Desktop\\wmts.xml";
         //HbaseConnection.getHBASEConn();
-        if(args.length == 0) {
+        if (args.length == 0) {
             System.out.println("Usage:\t--getLayers [-f xml_url]\n\t--deleteLayer layername start end\n\t--scanLayer layername start end\"");
-        }else if (args[0].equals("--getLayers")) {
+        } else if (args[0].equals("--getLayers")) {
             if (args.length == 3) {
                 if (args[1].equals("-f"))
                     url = args[2];
             }
             Document document = XMLP.parse(new URL(url));
             XMLP.getLayerNames(document);
-        }else if (args[0].equals("--scanRows")) {
+        } else if (args[0].equals("--scanRows")) {
             String layerName = args[1];
-            int start = Integer.valueOf(args[args.length-2]);
-            int end = Integer.valueOf(args[args.length-1]);
+            int start = Integer.valueOf(args[args.length - 2]);
+            int end = Integer.valueOf(args[args.length - 1]);
             for (int z = start; z <= end; ++z) {
                 //QuadTreeUtil.xyz2QuadTreeCodes(XMLP.getLayerInfo(document, "EPSG:4326", layerName, z), z,layerName);
                 String prefix = String.format("%02d", z);
-                HbaseUtils.scanRows("hbase_tile_table", layerName+prefix);
+                HbaseUtils.scanRows("hbase_tile_table", layerName + prefix);
             }
-        }else if (args[0].equals("--deleteLayer")) {
+        } else if (args[0].equals("--deleteLayer")) {
             String layerName = args[1];
-            int start = Integer.valueOf(args[args.length-2]);
-            int end = Integer.valueOf(args[args.length-1]);
-            for (int z = start; z <= end; ++z) {
-                //QuadTreeUtil.xyz2QuadTreeCodes(XMLP.getLayerInfo(document, "EPSG:4326", layerName, z), z,layerName);
-                String prefix = String.format("%02d", z);
-                HbaseUtils.deleteRows("hbase_tile_table", layerName+prefix);
+            if (args.length < 4)
+                System.out.println("Please identify the start and end !");
+            else {
+                int start = Integer.valueOf(args[args.length - 2]);
+                int end = Integer.valueOf(args[args.length - 1]);
+                for (int z = start; z <= end; ++z) {
+                    //QuadTreeUtil.xyz2QuadTreeCodes(XMLP.getLayerInfo(document, "EPSG:4326", layerName, z), z,layerName);
+                    String prefix = String.format("%02d", z);
+                    HbaseUtils.deleteRows("hbase_tile_table", layerName + prefix);
+                }
             }
         }
 
